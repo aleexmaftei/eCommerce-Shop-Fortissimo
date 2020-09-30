22f9957b11cf0a615e7c6660faa6c49d53f2c482
Alex M <48068999+aleexmaftei@users.noreply.github.com>
2020-09-30 10:37:27 +0300

    My profile

---
+++ eCommerce/Models/MyProfileVM/DeliveryLocationVm.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models.MyProfileVM
+{
+    public class DeliveryLocationVm
+    {
+        [Required(ErrorMessage = "Nume obligatoriu")]
+        [DisplayName("Country")]
+        public string CountryName { get; set; }
+
+        [Required(ErrorMessage = "Regiune obligatoriu")]
+        [DisplayName("Region")]
+        public string RegionName { get; set; }
+
+        [Required(ErrorMessage = "Oras obligatoriu")]
+        [DisplayName("City")]
+        public string CityName { get; set; }
+
+        [Required(ErrorMessage = "Adresa obligatoriu")]
+        [DisplayName("Adress")]
+        public string AddressDetail { get; set; }
+
+        [Required(ErrorMessage = "Cod Postal obligatoriu")]
+        [DisplayName("Postal Code")]
+        public string PostalCode { get; set; }
+    }
+}
--- eCommerce/Models/MyProfileVM/MyProfileVm.cs
+++ eCommerce/Models/MyProfileVM/MyProfileVm.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 
 namespace eCommerce.Models.MyProfileVM
 {
@@ -8,24 +6,6 @@ namespace eCommerce.Models.MyProfileVM
     {
         public List<InvoiceVM> UserInvoices { get; set; }
 
-        [Required(ErrorMessage = "Nume obligatoriu")]
-        [DisplayName("Country")]
-        public string CountryName { get; set; }
-
-        [Required(ErrorMessage = "Regiune obligatoriu")]
-        [DisplayName("Region")]
-        public string RegionName { get; set; }
-
-        [Required(ErrorMessage = "Oras obligatoriu")]
-        [DisplayName("City")]
-        public string CityName { get; set; }
-
-        [Required(ErrorMessage = "Adresa obligatoriu")]
-        [DisplayName("Adress")]
-        public string AddressDetail { get; set; }
-
-        [Required(ErrorMessage = "Cod Postal obligatoriu")]
-        [DisplayName("Postal Code")]
-        public string PostalCode { get; set; }
+        public List<DeliveryLocationVm> DeliveryLocations { get; set; }
     }
 }
